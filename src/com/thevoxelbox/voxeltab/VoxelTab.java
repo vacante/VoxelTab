c5b7a650436b7f6a362ca72303f6e7a8f798ac17
Vacante <mc.Vacante5.drew@gmail.com>
2013-03-13 15:59:37 -0400

    Fixed and re-arranged a few things

--- src/com/thevoxelbox/voxeltab/VoxelTab.java
+++ src/com/thevoxelbox/voxeltab/VoxelTab.java
@@ -1,11 +1,14 @@
-package com.thevoxelbox.VoxelTab;
+package com.thevoxelbox.voxeltab;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 /*
  * @Author Drew
@@ -15,34 +18,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class VoxelTab extends JavaPlugin
   implements Listener
 {
-	public void onEnable()
+	public void onEnable1()
 	  {
-	    loadConfig();
+	    getConfig();
 	    getServer().getPluginManager().registerEvents(this, this);
 	    System.out.println("VoxelTab Loaded");
-	    System.out.printIn("| ========================================== |");
-	    System.out.printIn("| * VoxelTab                                 |");
-	    System.out.printIn("| *                                          |");
-	    System.out.printIn("| * Basic Tab Based coloring plugin          |");
-	    System.out.printIn("| *                                          |");
-	    System.out.printIn("| * Drew and VoxelPlugineering               |");
-	    System.out.printIn("| ========================================== |");
-	    System.out.printIn("Factory settings loaded");
-	  }
+	    System.out.print("| ========================================== |");
+	    System.out.print("| * VoxelTab                                 |");
+	    System.out.print("| *                                          |");
+	    System.out.print("| * Basic Tab Based coloring plugin          |");
+	    System.out.print("| *                                          |");
+	    System.out.print("| * Drew and VoxelPlugineering               |");
+	    System.out.print("| ========================================== |");
+	    System.out.print("Factory settings loaded");
 	  }
 
 	public void onDisable()
 	  {
 	    System.out.println("VoxelTab has been disabled");
 	    System.out.println("VoxelTab Unloaded");
-	    System.out.printIn("| ========================================== |");
-	    System.out.printIn("| * VoxelTab                                 |");
-	    System.out.printIn("| *                                          |");
-	    System.out.printIn("| * Basic Tab Based coloring plugin          |");
-	    System.out.printIn("| *                                          |");
-	    System.out.printIn("| * Drew and VoxelPlugineering               |");
-	    System.out.printIn("| ========================================== |");
-	    System.out.printIn("Factory settings Unloaded");
+	    System.out.print("| ========================================== |");
+	    System.out.print("| * VoxelTab                                 |");
+	    System.out.print("| *                                          |");
+	    System.out.print("| * Basic Tab Based coloring plugin          |");
+	    System.out.print("| *                                          |");
+	    System.out.print("| * Drew and VoxelPlugineering               |");
+	    System.out.print("| ========================================== |");
+	    System.out.print("Factory settings Unloaded");
 	  }
 	
 	@Override
